df718719f3829f9c3a33da98be58dc9c062de90a
CKScrips <levelupp25@yahoo.com>
2024-08-28 20:41:06 -0400

    Update Games-List.lua

--- Games-List/Games-List.lua
+++ Games-List/Games-List.lua
@@ -1,11 +1,11 @@
 local Games = {
-    [4777817887] = "https://raw.githubusercontent.com/CKScrips/CK-Hub/main/games/Blade-Ball.lua",
-    [3317771874] = "https://raw.githubusercontent.com/CKScrips/CK-Hub/main/games/Pet-Sim_99.lua",
-    [994732206] = "https://raw.githubusercontent.com/CKScrips/CK-Hub/main/games/Blox-Fruit.lua",
+    [4777817887] = "https://raw.githubusercontent.com/CKScrips/HubBuild/main/Games/Blade-Ball.lua",
+    [3317771874] = "https://raw.githubusercontent.com/CKScrips/HubBuild/main/Games/Pet-Sim-99.lua",
+    [994732206] = "https://raw.githubusercontent.com/CKScrips/HubBuild/main/Games/Blox-Fruit.lua",
     [383310974] = "https://raw.githubusercontent.com/CKScrips/HubBuild/main/Games/adopt-me.lua",
-    [66654135] = "https://raw.githubusercontent.com/CKScrips/CK-Hub/main/games/MM2.lua",
-    [1008451066] = "https://raw.githubusercontent.com/CKScrips/CK-Hub/main/games/Da-Hood.lua",
-    [2316994223] = "https://raw.githubusercontent.com/CKScrips/CK-Hub/main/games/Pet-Sim-X.lua",
+    [66654135] = "https://raw.githubusercontent.com/CKScrips/HubBuild/main/Games/MM2.lua",
+    [1008451066] = "https://raw.githubusercontent.com/CKScrips/HubBuild/main/Games/Da-Hood.lua",
+    [2316994223] = "https://raw.githubusercontent.com/CKScrips/HubBuild/main/Games/Pet-Sim-X.lua",
 }
 
 return Games
